5b28620b068403f38452fffb66a662340cd4a3a3
tr1zet <148443756+tr1zet@users.noreply.github.com>
2026-04-09 09:53:28 +0000

    commit 2

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 # Flask application
 from flask import Flask
+import socket
 
 app = Flask(__name__)
 
@@ -7,5 +8,14 @@ app = Flask(__name__)
 def hello():
     return 'Hello from Codespaces!'
 
+def find_free_port():
+    """Находит свободный порт"""
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('', 0))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return s.getsockname()[1]
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = find_free_port()
+    print(f"Запуск на порту: {port}")
+    app.run(debug=True, port=port)
